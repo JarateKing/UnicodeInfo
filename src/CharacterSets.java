2469e8796e43f4766d3c99d7b2a1845bdb90be4d
JarateKing <scmacintosh@upei.ca>
2018-01-19 09:00:58 -0400

    added character set object

---
+++ src/CharacterSets.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class CharacterSets {
+    private ArrayList<CharacterList> sets;
+
+    public CharacterSets()
+    {
+        sets = new ArrayList();
+        sets.add(new CharsASCII());
+    }
+
+    public String listSetsUsed(String input)
+    {
+        String base = "";
+
+        for (int i = 0; i < sets.size(); i++)
+        {
+            for (int j = 0; j < input.length(); j++)
+            {
+                if (sets.get(i).isCharacterInList(input.charAt(j)))
+                {
+                    base += sets.get(i) + "\n";
+                    break;
+                }
+            }
+        }
+
+        return base;
+    }
+}
