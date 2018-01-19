2f4df97114c3f0e97efc147f98095ba02cc40450
JarateKing <scmacintosh@upei.ca>
2018-01-19 09:03:48 -0400

    added basic driver test

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,8 @@
 public class Driver {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        CharacterSets sets = new CharacterSets();
+
+        System.out.print(sets.listSetsUsed("Hello World!"));
     }
 }
