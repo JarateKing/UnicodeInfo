e49bb4a26f2ab2e0fa807422bbe5fe214e56c58c
JarateKing <scmacintosh@upei.ca>
2018-02-04 12:04:05 -0400

    added regex special characters

--- src/CharArrays.java
+++ src/CharArrays.java
@@ -1,5 +1,7 @@
 public enum CharArrays implements CharacterList
 {
+    REGEX_SPECIAL (new char[]{'$', '(', ')', '*', '+', '.', '?', '[', '^', '\\', '{', '|'}),
+
     FRACTIONS (new char[]{0xBC, 0xBD, 0xBE, 0x2150, 0x2151, 0x2152, 0x2153, 0x2154,
                           0x2155, 0x2156, 0x2157, 0x2158, 0x2159, 0x215A, 0x215B,
                           0x215C, 0x215D, 0x215E}),
