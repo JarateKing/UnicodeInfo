388d5c9e3300e410ee6bbf4b847e6dde5ee06ebe
JarateKing <scmacintosh@upei.ca>
2018-02-05 09:07:17 -0400

    showcase more functions on driver

--- src/Driver.java
+++ src/Driver.java
@@ -3,6 +3,13 @@ public class Driver {
     {
         CharacterSets sets = new CharacterSets();
 
-        System.out.print(sets.listSetsUsed("Hello World!"));
+        System.out.println("Sets used in \"Hello World!\":");
+        System.out.println(sets.listSetsUsed("Hello World!"));
+
+        System.out.println("Regex of the regex special characters:");
+        System.out.println(CharArrays.REGEX_SPECIAL.getRegex());
+
+        System.out.println("Are there zero-width spaces in \"right\u200Bhere\":");
+        System.out.println(sets.isSetUsed(CharArrays.ZERO_WITDH, "right\u200Bhere"));
     }
 }
