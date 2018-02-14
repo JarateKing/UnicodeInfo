9fa99eaaf7ae18c6edf5175ab2edf0f56ad5c780
JarateKing <scmacintosh@upei.ca>
2018-02-14 11:59:41 -0400

    added character combinations

---
+++ src/CharCombinations.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public enum CharCombinations implements CharacterList
+{
+    private final CharacterList[] list;
+
+    CharCombinations(CharacterList[] list)
+    {
+        this.list = list;
+    }
+
+    public char[] all()
+    {
+        return null;
+    }
+
+    public boolean isCharacterInList(char value)
+    {
+        for (int i = 0; i < list.length; i++)
+        {
+            if (list[i].isCharacterInList(value))
+                return true;
+        }
+        return false;
+    }
+
+    public String getRegex()
+    {
+        StringBuilder regex = new StringBuilder();
+
+        // add the first character
+        regex.append(list[0].getRegex());
+
+        // add the other characters with a | in front
+        for(int i = 1; i < list.length; i++) {
+            regex.append("|");
+            regex.append(list[i].getRegex());
+        }
+
+        return regex.toString();
+    }
+}
