8baa8975bf1a86724148ec8b962711e41cc2ed9f
JarateKing <scmacintosh@upei.ca>
2018-02-17 14:10:23 -0400

    added greek to ranges

--- src/CharRanges.java
+++ src/CharRanges.java
@@ -5,6 +5,7 @@ public enum CharRanges implements CharacterList
     LATIN_SUPPLEMENT (0xA0, 0xFF),
     LATIN_EXTENDED_A (0x100, 0x17F),
     LATIN_EXTENDED_B (0x180, 0x24F),
+    GREEK (0x370, 0x3FF),
     CYRILLIC_BASIC (0x400, 0x4FF),
     CYRILLIC_SUPPLEMENT (0x500, 0x52F);
 
